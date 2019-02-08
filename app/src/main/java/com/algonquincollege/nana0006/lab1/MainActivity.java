fd37fbf45a8ee01ba314ecc2a7409d24b9705562
TheBitcoinKing <nanangassa@yahoo.fr>
2019-02-08 00:57:07 -0500

    Lab3 Ready

--- app/src/main/java/com/algonquincollege/nana0006/lab1/MainActivity.java
+++ app/src/main/java/com/algonquincollege/nana0006/lab1/MainActivity.java
@@ -1,13 +1,104 @@
 package com.algonquincollege.nana0006.lab1;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Email = "emailKey";
+    EditText txtUsername, txtPassword, txtEmail;
+    Button buttonReg2;
+
+   // String Password ;
+    //String email ;
+
+    SharedPreferences.Editor editor;
+    SharedPreferences sharedPreferences;
+
+   // String name = txtUsername.getText().toString();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_lab3);
+
+        txtPassword = (EditText) findViewById(R.id.password);
+        txtEmail = (EditText) findViewById(R.id.email);
+        buttonReg2 = (Button) findViewById(R.id.button);
+
+        buttonReg2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent= new Intent(MainActivity.this,ProfileActivity.class);
+                intent.putExtra("key1",  txtEmail.getText().toString());
+                intent.putExtra("key2", txtPassword.getText().toString());
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        sharedPreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+        if (sharedPreferences.contains(Name)) {
+            txtEmail.setText(sharedPreferences.getString(Name, ""));
+        }
+        if (sharedPreferences.contains(Name)) {
+            txtPassword.setText(sharedPreferences.getString(Email, ""));
+
+       }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String pass = txtPassword.getText().toString();
+        String mail = txtEmail.getText().toString();
+
+       // final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(Name, mail);
+        editor.putString(Email, pass);
+        editor.commit();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/algonquincollege/nana0006/lab1/ProfileActivity.java
@@ -0,0 +1,103 @@
+package com.algonquincollege.nana0006.lab1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+
+public class ProfileActivity extends AppCompatActivity{
+
+  //  private static final String ACTIVITY_NAME = "ProfileActivity";
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView mImageButton;
+    EditText mail ;
+    EditText pass ;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity_lab3);
+        mail = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.password);
+        mImageButton = (ImageButton) findViewById(R.id.button);
+
+        mail.setText(getIntent().getStringExtra("key1"));
+        pass.setText(getIntent().getStringExtra("key2"));
+
+        mImageButton.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                //Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show();
+            dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
+
+    }
+        @Override
+    protected void onStart() {
+            super.onStart();
+            Log.e("ProfileActivity", "In function: " + "onStart");
+        }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+                Log.e("ProfileActivity", "In function: " + "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("ProfileActivity", "In function: " + "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("ProfileActivity", "In function: " + "onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("ProfileActivity", "In function: " + "onDestroy");
+
+    }
+
+ //   @Override
+ //   protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+ //       super.onActivityResult(requestCode, resultCode, data);
+ //   }
+}
